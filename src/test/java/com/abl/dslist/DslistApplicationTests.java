3098e154cb85ae2df66bc40276e12e1b92cb8b26
Alessandro Bruno Lima <alessandrobrunolima@gmail.com>
2025-08-18 22:58:21 -0300

    Project created

---
+++ src/test/java/com/abl/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abl.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
