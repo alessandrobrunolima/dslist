bc7ad364f46b00b3ce182a65cfcf48c4325e3903
Alessandro Bruno Lima <alessandrobrunolima@gmail.com>
2025-08-20 23:52:18 -0300

    Adiciona endpoints para gerenciamento de jogos e DTO para detalhamento de jogos

--- src/main/java/com/abl/dslist/controllers/GameController.java
+++ src/main/java/com/abl/dslist/controllers/GameController.java
@@ -4,23 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abl.dslist.dto.GameDTO;
 import com.abl.dslist.dto.GameMinDTO;
 import com.abl.dslist.services.GameService;
-
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
-  
+
 	@Autowired
 	private GameService gameService;
 
 	@GetMapping
-	public List<GameMinDTO> findAll(){
+	public List<GameMinDTO> findAll() {
 		return gameService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		return gameService.findById(id);
 	}
 
 }
--- src/main/java/com/abl/dslist/services/GameService.java
+++ src/main/java/com/abl/dslist/services/GameService.java
@@ -1,11 +1,15 @@
 package com.abl.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.abl.dslist.dto.GameDTO;
 import com.abl.dslist.dto.GameMinDTO;
+import com.abl.dslist.entities.Game;
 import com.abl.dslist.repositories.GameRepository;
 
 @Service
@@ -14,10 +18,20 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		return gameRepository.findAll().stream()
 				.map(game -> new GameMinDTO(game))
 				.toList();
+	}
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Optional<Game> game = gameRepository.findById(id);
+		if (!game.isPresent()) {
+			throw new RuntimeException("Game not found");
+		}
+		return new GameDTO(game.get());
 	}
 
 }
